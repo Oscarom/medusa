fdbcfc2c4d34810df08b28ec5d0de28934b622f7
Oscar Omar Gonzalez Toledo <oscar.gonzalez@tautenet.com.mx>
2023-12-18 15:39:47 -0600

    renderizado imagen producto

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,38 +1,73 @@
 // screens/HomeScreen.js
 import React, { useEffect, useState } from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from "react-native";
 import { SearchBar } from "react-native-elements";
-import { Button, Card, Title, Paragraph } from "react-native-paper";
+import { Title, Paragraph } from "react-native-paper";
+import { Text, Card, Button, Icon } from "@rneui/themed";
+
 import axios from "axios";
 
 const HomeScreen = () => {
-  
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Cambia la URL según la dirección de tu servidor Express
-    const apiUrl = 'http://192.168.1.68:3000/api/users';
+    const apiUrl = "http://192.168.1.68:3000/api/productos";
 
-    axios.get(apiUrl)
-      .then(response => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
         setUsers(response.data);
       })
-      .catch(error => {
-        console.error('Error al obtener datos del servidor: ', error);
+      .catch((error) => {
+        console.error("Error al obtener datos del servidor: ", error);
       });
   }, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <SearchBar placeholder="Buscar..." />
-        <Text></Text>
-
-        <Text>Datos de la base de datos MySQL:</Text>
-        {users.map((user) => (
-          <Text key={user.id}>Id:{user.id} Nombre:{user.nombre} Email:{user.email}</Text>
-        ))}
-
+    <ScrollView>
+      <SafeAreaView>
+        <View>
+          <SearchBar placeholder="Buscar..." />
+          <Text></Text>
+          {users.map((user) => (
+            <Card key={user.id}>
+              <Card.Title>{user.nombre}</Card.Title>
+              <Card.Divider />
+              <Card.Image
+  
+                style={{ padding: 170, width:150, alignContent:"center"}}
+                source={{
+                  uri:"https://ibarramayoreo.com/images/IMAGENES/5895/01.jpg",
+                }}
+              />
+              <Text style={{ marginBottom: 10, marginTop:10, textAlign:"center" }}>
+                Precio:{user.precio}
+              </Text>
+              <Text style={{ marginBottom: 10, textAlign:"center" }}>
+                Supermercado:{user.nombre_superm}
+              </Text>
+              <Text style={{ marginBottom: 10, textAlign:"center" }}>
+                Categoria:{user.categoria}
+              </Text>
+              <Button
+                buttonStyle={{
+                  borderRadius: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginBottom: 0,
+                }}
+                title="Ver detalle"
+              />
+            </Card>
+          ))}
+          {/* 
         <Card>
           <Card.Cover
             source={{
@@ -45,11 +80,12 @@ const HomeScreen = () => {
               Visita las ofertas
             </Button>
           </Card.Content>
-        </Card>
+        </Card> */}
 
-        <Text></Text>
-      </View>
-    </SafeAreaView>
+          <Text></Text>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
